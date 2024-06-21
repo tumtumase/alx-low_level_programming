2c72a1f84302eb86de302927c639d91d4f622061
tumtumase <moecabolerato@gmail.com>
2024-06-21 11:57:20 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int i;
+
+    for (i = 0; i < 16; i++)
+    {
+        if (i < 10)
+            putchar('0' + i);
+        else
+            putchar('a' + (i - 10));
+    }
+    putchar('\n');
+
+    return (0);
+}
