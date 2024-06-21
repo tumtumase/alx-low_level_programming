d5d9253f6b26a872e97dc74b57ae96805e5d7fa8
tumtumase <moecabolerato@gmail.com>
2024-06-21 12:05:02 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,18 @@
  */
 int main(void)
 {
-    int num;
+	int num;
 
-    for (num = 0; num <= 9; num++)
-    {
-        putchar('0' + num);
-        if (num < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
-    }
-    putchar('\n');
+	for (num = 0; num <= 9; num++)
+	{
+		putchar('0' + num);
+		if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
