1397344bb592e6994e65c65a063140b76c858af8
tumtumase <moecabolerato@gmail.com>
2024-06-21 11:48:50 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 
-int main(void) {
-    for (int i = 0; i <= 9; i++) {
-        putchar('0' + i);
-        putchar('\n');
-    }
+int main(void)
+{
+    // Using putchar to print digits 0 to 9 directly
+    putchar(48); // ASCII value of '0'
+    putchar(49); // ASCII value of '1'
+    putchar(50); // ASCII value of '2'
+    putchar(51); // ASCII value of '3'
+    putchar(52); // ASCII value of '4'
+    putchar(53); // ASCII value of '5'
+    putchar(54); // ASCII value of '6'
+    putchar(55); // ASCII value of '7'
+    putchar(56); // ASCII value of '8'
+    putchar(57); // ASCII value of '9'
+    putchar('\n'); // Print newline character
+
     return 0;
 }
