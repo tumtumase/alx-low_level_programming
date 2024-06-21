5e9f3a3dceea7f5b7a16bbb980284e37a453919f
tumtumase <moecabolerato@gmail.com>
2024-06-21 11:31:21 -0700

    6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -2,16 +2,17 @@
 
 int main(void)
 {
-    putchar('0');
-    putchar('1');
-    putchar('2');
-    putchar('3');
-    putchar('4');
-    putchar('5');
-    putchar('6');
-    putchar('7');
-    putchar('8');
-    putchar('9');
+    /* Using putchar to print single digits 0 to 9 */
+    putchar('0' + 0);
+    putchar('0' + 1);
+    putchar('0' + 2);
+    putchar('0' + 3);
+    putchar('0' + 4);
+    putchar('0' + 5);
+    putchar('0' + 6);
+    putchar('0' + 7);
+    putchar('0' + 8);
+    putchar('0' + 9);
     putchar('\n');
 
     return 0;
