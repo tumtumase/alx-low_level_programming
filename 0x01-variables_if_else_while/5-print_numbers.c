5a9489da21ee845a8d79b5c15de8fbe816098a64
tumtumase <moecabolerato@gmail.com>
2024-06-21 11:12:23 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints all single digit numbers of base 10
+ * starting from 0, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    int number = 0;
+
+    while (number < 10)
+    {
+        putchar(number + '0'); // Convert number to corresponding character
+        number++;
+    }
+
+    putchar('\n');
+
+    return (0);
+}
