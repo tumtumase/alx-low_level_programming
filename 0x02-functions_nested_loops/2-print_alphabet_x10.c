76e5a441eab80d9191691318d9a54cfe7230aaaf
tumtumase <moecabolerato@gmail.com>
2024-06-26 10:55:30 -0700

    10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,18 +6,18 @@
  */
 void print_alphabet_x10(void)
 {
-    char ch;
-    int i;
+	char ch;
+	int i;
 
-    i = 0;
+	i = 0;
 
-    while (i < 10)
-    {
-        for (ch = 'a'; ch <= 'z'; ch++)
-        {
-            _putchar(ch);
-        }
-        _putchar('\n');
-        i++;
-    }
+	while (i < 10)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+		_putchar('\n');
+		i++;
+	}
 }
