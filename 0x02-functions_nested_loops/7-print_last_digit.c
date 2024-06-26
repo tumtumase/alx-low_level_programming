9ddb39cc91f211480b53dacff503326f7b107370
tumtumase <moecabolerato@gmail.com>
2024-06-26 12:50:23 -0700

    7 last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,12 @@
  */
 int print_last_digit(int n)
 {
-    int last_digit = n % 10;
+	int last_digit = n % 10;
 
-    if (last_digit < 0)
-        last_digit = -last_digit;
+	if (last_digit < 0)
+		last_digit = -last_digit;
 
-    _putchar('0' + last_digit);
+	_putchar('0' + last_digit);
 
-    return last_digit;
+	return (last_digit);
 }
