df7e8c8cd951b347d13cb1d298b5ebbaf7b6bad3
tumtumase <moecabolerato@gmail.com>
2024-06-26 09:00:47 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,7 +3,7 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0 
  */
 int main(void)
 {
@@ -16,6 +16,5 @@ int main(void)
     _putchar('a');
     _putchar('r');
     _putchar('\n');
-
-    return (0);
+return (0);
 }
