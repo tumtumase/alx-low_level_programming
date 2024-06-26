3dc71f6813cb349f8ac733f599ca967b42a0c2a7
tumtumase <moecabolerato@gmail.com>
2024-06-26 13:57:25 -0700

    98 Battery Street, the OG again

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,12 +18,13 @@ void print_to_98(int n)
 	}
 	else
 	{
-		for (; n >= 98; n--)
+		for (; n > 98; n--)
 		{
 			printf("%d", n);
 			if (n != 98)
 				printf(", ");
 		}
+		printf("98");
 	}
 	printf("\n");
 }
