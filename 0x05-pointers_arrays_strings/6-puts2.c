3a6ef43f0e0e59b8416e1127825d7bb1de88b6f0
tumtumase <moecabolerato@gmail.com>
2024-07-01 12:39:53 -0700

    6 puts c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * puts2 - prints every other character of a string, starting with the first character
+ * puts2 - prints every other character of a string,
+ * starting with the first character
  * @str: pointer to the string
  * Return: void
  */
