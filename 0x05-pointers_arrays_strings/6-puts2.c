25208aa3cbe4efafdd8d903059f89d8268d38cc6
tumtumase <moecabolerato@gmail.com>
2024-07-01 12:47:36 -0700

    Pointers arrays

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,12 @@
  */
 void puts2(char *str)
 {
-	int i;
+int i = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
-	{
-		_putchar(str[i]);
-	}
-	_putchar('\n');
+while (str[i] != '\0')
+{
+_putchar(str[i]);
+i += 2;
+}
+_putchar('\n');
 }
