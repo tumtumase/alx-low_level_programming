57051362b2bb796a3105e2e03c93678107ae31cc
tumtumase <moecabolerato@gmail.com>
2024-07-04 09:33:20 -0700

    A dream doesn't become reality

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - prints even numbers from 0 to 100
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	printf("Before loop\n");
+	for (i = 0; i < 100; i++)
+	{
+		if (i % 2 != 0)
+		{
+			printf("i is not even so don't print\n");
+			continue;
+		}
+		else
+		{
+			printf("i is even, break to print\n");
+			printf("%d\n", i);
+			break;
+		}
+	}
+	printf("For loop exited\n");
+	return (0);
+}
