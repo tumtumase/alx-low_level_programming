3d2aeee7557b8ad74423b46413178a31f4b17215
tumtumase <moecabolerato@gmail.com>
2024-07-04 13:11:27 -0700

    C is fun prjt 12

---
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * print_buffer - Prints a buffer
+ * @b: The buffer to be printed
+ * @size: The number of bytes to be printed from the buffer
+ */
+void print_buffer(char *b, int size)
+{
+	int i, j;
+
+	if (size <= 0)
+	{
+		printf("\n");
+		return;
+	}
+
+	for (i = 0; i < size; i += 10)
+	{
+		printf("%08x: ", i);
+
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j < size)
+				printf("%02x", (unsigned char)b[i + j]);
+			else
+				printf("  ");
+
+			if (j % 2)
+				printf(" ");
+		}
+
+		for (j = 0; j < 10; j++)
+		{
+			if (i + j >= size)
+				break;
+
+			if (isprint(b[i + j]))
+				printf("%c", b[i + j]);
+			else
+				printf(".");
+		}
+
+		printf("\n");
+	}
+}
