75c6386f6ff899c4065ec8f1d36c3323e659a08a
tumtumase <moecabolerato@gmail.com>
2024-07-08 13:30:47 -0700

    Strspn ag

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -10,17 +10,17 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-    unsigned int i, j;
+	unsigned int i, j;
 
-    for (i = 0; s[i]; i++)
-    {
-        for (j = 0; accept[j]; j++)
-        {
-            if (s[i] == accept[j])
-                break;
-        }
-        if (!accept[j])
-            return (i);
-    }
-    return (i);
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			return (i);
+	}
+	return (i);
 }
