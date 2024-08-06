1cfcfab2783f5bc676b33fdbb5d470d832e66397
tumtumase <moecabolerato@gmail.com>
2024-08-06 13:15:17 -0700

    print listint

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -12,11 +12,10 @@
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-/* Function prototype */
 size_t print_listint(const listint_t *h);
 
 #endif /* LISTS_H */
